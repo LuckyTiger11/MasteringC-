4b4078dd196c1d720c078b305d0b4cd6af797eec
Aleksandr Yeganov <ayeganov@gmail.com>
2016-07-24 13:36:01 -0400

    Minimal changes to create a pull request

--- PCP_Chapter5_Exercises.cpp
+++ PCP_Chapter5_Exercises.cpp
@@ -30,75 +30,75 @@ double km_ml(double kilometers); // converts kilometers to miles
 
 int _tmain(int argc, _TCHAR* argv[])
 { 
-	//5-1
-	double cel, fah;
+    //5-1
+    double cel, fah;
 
-	cout << "\tCelcius to Fahrenheit (5-1)\n\n" << "Enter temperature (in celcius) you wish to convert: "; // promps user for a temperature in celcius to be converted into farenheit
-	cin >> cel;
+    cout << "\tCelcius to Fahrenheit (5-1)\n\n" << "Enter temperature (in celcius) you wish to convert: "; // promps user for a temperature in celcius to be converted into farenheit
+    cin >> cel;
 
-	fah = cel_fah(cel); // function that converts temperature is called
-	
-	cout << cel << " degrees C is " << fah << " degrees F\n\n"; // prints result of convertion
+    fah = cel_fah(cel); // function that converts temperature is called
     
-	//5-2
-	float radius, volume;
+    cout << cel << " degrees C is " << fah << " degrees F\n\n"; // prints result of convertion
+    
+    //5-2
+    float radius, volume;
 
-	cout << "\tSphere volume (5-2)\n\n" << "Enter radius(m) of the sphere: ";
-	cin >> radius;
+    cout << "\tSphere volume (5-2)\n\n" << "Enter radius(m) of the sphere: ";
+    cin >> radius;
 
-	volume = sphere_vol(radius);
+    volume = sphere_vol(radius);
 
-	cout << volume << "m^3 is the volume of the sphere \n\n";
+    cout << volume << "m^3 is the volume of the sphere \n\n";
 
-	//5-3
-	int width, height;
+    //5-3
+    int width, height;
 
-	cout << "\tPerimeter of Rectangle (5-3)\n\n" << "Enter its width and height (separated by space): "; // prompts user for both width and height
-	cin >> width >> height; // decided to use cin with several parameters and see functionality.
+    cout << "\tPerimeter of Rectangle (5-3)\n\n" << "Enter its width and height (separated by space): "; // prompts user for both width and height
+    cin >> width >> height; // decided to use cin with several parameters and see functionality.
 
-	cout << "The perimeter of the rectangle is: " << rec_perim(width, height) << "\n\n"; // calculates & prints out resulting perimeter
+    cout << "The perimeter of the rectangle is: " << rec_perim(width, height) << "\n\n"; // calculates & prints out resulting perimeter
 
-	//5-4
-	double kilo;
-	cout << "\tKilometers/h to Miles/h (5-4)\n\n" << "Enter how fast you are going (Km/h): ";
-	cin >> kilo;
+    //5-4
+    double kilo;
+    cout << "\tKilometers/h to Miles/h (5-4)\n\n" << "Enter how fast you are going (Km/h): ";
+    cin >> kilo;
 
-	cout << "you would be going at " << km_ml(kilo) << " Miles/h! \n\n";
-	
+    cout << "you would be going at " << km_ml(kilo) << " Miles/h! \n\n";
+    
 
-	//5-5
-	int time[2];
-	cout << "\tHours and Minutes to just Minutes (5-5)\n\n" << "Enter your hours and minutes (separated by space): ";
-	cin >> time[0] >> time[1];
+    //5-5
+    int time[2];
+    cout << "\tHours and Minutes to just Minutes (5-5)\n\n" << "Enter your hours and minutes (separated by space): ";
+    cin >> time[0] >> time[1];
 
-	cout << "That is " << time[0] * 60 + time[1] << " minutes! \n\n"; // I'm getting good. 
-
-
-	//5-6
-	int hour_min;
-	cout << "\tMinutes to Hour and Minutes (5-6)\n\n" << "Enter your Minutues: ";
-	cin >> hour_min;
-
-	cout << "That is " << hour_min / 60 << " Hours and " << hour_min % 60 << " Minutes!\n\n"; // Ima beast! 
+    cout << "That is " << time[0] * 60 + time[1] << " minutes! \n\n"; // I'm getting good. 
 
 
-	return 0;
+    //5-6
+    int hour_min;
+    cout << "\tMinutes to Hour and Minutes (5-6)\n\n" << "Enter your Minutues: ";
+    cin >> hour_min;
+
+    cout << "That is " << hour_min / 60 << " Hours and " << hour_min % 60 << " Minutes!\n\n"; // Ima beast! 
+
+
+    return 0;
 }
 
-double	cel_fah(double celcius) // body of the function that converts degrees in celcius to farenheit
+double  cel_fah(double celcius) // body of the function that converts degrees in celcius to farenheit
 {
-	double fahrenheit;
+    double fahrenheit;
 
-	return fahrenheit = (static_cast<double>(9)/5 * celcius) + 32; // C++ will return an integer as a result of the division 9/5 (1.8 becomes 1, chops the decimal off!). Using 9.0 would have yield
-														           // the same result. Static_cast converts the variable or number to the parameter assign. I used this opportunity to try it.
+    return fahrenheit = (static_cast<double>(9)/5 * celcius) + 32; // C++ will return an integer as a result of the division 9/5 (1.8 becomes 1, chops the decimal off!). Using 9.0 would have yield
+                                                                   // the same result. Static_cast converts the variable or number to the parameter assign. I used this opportunity to try it.
 }
 
 float sphere_vol(float radius) // body of function that calculates the sphere's volume
 {
-	const double PI = 3.1415926;
-	float volume;
-	
-	return volume = static_cast<double>(4)/3 * PI * pow (radius, 3);  // pow() is used to calculate radius*e^3 (math.h library needed).  
+    const double PI = 3.1415926;
+    float volume;
+    
+    return volume = static_cast<double>(4)/3 * PI * pow (radius, 3);  // pow() is used to calculate radius*e^3 (math.h library needed).  
 }
 
 int rec_perim(int p_height, int p_width)  // fast and easy function that calculates perimeters of rectangles
